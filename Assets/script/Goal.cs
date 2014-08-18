584be87924a7733cc15b36c39701ea699bccb2b7
KentaTsunefuka <std0dc00@gmail.com>
2014-08-18 16:28:48 +0900

    ゴール判定追加。

---
+++ Assets/script/Goal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+	public GameObject targetObject;
+	
+	private void OnTriggerEnter(Collider collider) {
+		Debug.Log("target: " + this.targetObject.name);
+		Debug.Log("hit: " + collider.name);
+		if (targetObject.name == collider.name) {
+			Debug.Log("goal: ");			
+		}
+	}	
+}
