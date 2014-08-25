2cda7b2744e77eb2206a3f27bd2d542d3e2740a8
KentaTsunefuka <std0dc00@gmail.com>
2014-08-25 09:55:35 +0900

    弾が当たったら弾をdestroyするスクリプト追加

---
+++ Assets/script/KillBullet.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillBullet : MonoBehaviour {
+	
+	public string targetTag;
+
+	private void OnTriggerEnter(Collider collider) {
+		if (collider.tag == targetTag) {
+		    Destroy(collider.gameObject);			
+		}
+	}
+
+}
